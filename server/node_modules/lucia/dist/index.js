751f2ae4321ccab2266b66b80177dab52c8e807a
demuesymitchell <mitchdemuesy@gmail.com>
2026-05-03 12:01:38 -0400

    fix: remove node_modules from git

--- server/node_modules/lucia/dist/index.js
+++
@@ -1,5 +0,0 @@
-export { Lucia } from "./core.js";
-export { Scrypt, LegacyScrypt, generateId, generateIdFromEntropySize } from "./crypto.js";
-export { TimeSpan } from "./date.js";
-export { Cookie } from "./cookie.js";
-export { verifyRequestOrigin } from "./request.js";
